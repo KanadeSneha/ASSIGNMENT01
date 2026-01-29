09e53051d08d0f21385002010b1731ca3a4bb6d0
KanadeSneha <sneham27@gmail.com>
2026-01-29 12:53:26 +0000

    updated customworld

--- src/tests/steps/landingPageSteps.ts
+++ src/tests/steps/landingPageSteps.ts
@@ -1,6 +1,7 @@
 import { Given, Then, When, setDefaultTimeout } from "@cucumber/cucumber"
 import { expect } from "@playwright/test";
 import { getCurrentDateISO, getPastDateISO, getFutureDateISO } from '../support/dateUtils'
+import { CustomWorld } from "../support/world";
 
 const errorMessages: Record<string, string> = {
   'max length': 'More than 100 characters not allowed',
@@ -11,11 +12,11 @@ const errorMessages: Record<string, string> = {
 };
 setDefaultTimeout(60 * 1000);
 
-Given('I navigate to {string}', async function (url: string) {
+Given('I navigate to {string}', async function (this:CustomWorld,url: string) {
   await this.page.goto(url);
 });
 
-Then('I should be able to see dashborad page', async function () {
+Then('I should be able to see dashborad page', async function (this:CustomWorld) {
   console.log("Verifying dashboard page");
   await this.page.waitForLoadState('networkidle');
   expect(this.page.url()).toContain('todo.testing.groupgti.com');
@@ -25,31 +26,30 @@ Then('I should be able to see dashborad page', async function () {
   console.log(`Page title: ${title}`);
 });
 
-Given('I click on ToDo content textbox', async function () {
+Given('I click on ToDo content textbox', async function (this:CustomWorld) {
   await this.page.getByRole('textbox', { name: 'Todo content' }).click();
   await this.page.locator('#mat-input-1').click();
 });
 
 
-Then('I should see {string} error message',
-  async function (errorType: string) {
+Then('I should see {string} error message', async function (this:CustomWorld,errorType: string) {
     const expectedMessage = errorMessages[errorType];
     const errorText = await this.page.locator('#mat-mdc-error-0').textContent();
     expect(errorText).toContain(expectedMessage);
   }
 );
 
-Given('I enter text content {string}', async function (text: string) {
+Given('I enter text content {string}', async function (this:CustomWorld,text: string) {
   await this.page.getByRole('textbox', { name: 'Todo content' }).click();
   await this.page.getByRole('textbox', { name: 'Todo content' }).fill(text);
 });
 
-When('I click on Create Todo button', async function () {
+When('I click on Create Todo button', async function (this:CustomWorld) {
   await this.page.getByRole('button', { name: 'Create ToDo' }).click();
 
 });
 
-Then('I should be able to see {string} in ToDo item to list', async function (text: string) {
+Then('I should be able to see {string} in ToDo item to list', async function (this:CustomWorld,text: string) {
   const grabbedText = await this.page.locator('table').locator(`text="${text}"`).textContent();
   expect(grabbedText).toContain(text);;
 });
@@ -59,20 +59,20 @@ Then('I should see error message', async function () {
   expect(errortext).toContain('More than 100 characters not allowed');
 });
 
-When('I click on Due date field', async function () {
+When('I click on Due date field', async function (this:CustomWorld) {
   await this.page.locator('#mat-input-1').click();
 });
 
 
-When('clear the Due date', async function () {
+When('clear the Due date', async function (this:CustomWorld) {
   await this.page.locator('#mat-input-1').fill('');
 });
 
-When('add current date', async function () {
+When('add current date', async function (this:CustomWorld) {
   await this.page.fill('#mat-input-1', getCurrentDateISO());
 });
 
-When('I select a due date {int} days in the past', async function (days: number) {
+When('I select a due date {int} days in the past', async function (this:CustomWorld, days: number) {
   const pastDate = getPastDateISO(days);
   await this.page.fill('#mat-input-1', pastDate);
 });
@@ -82,6 +82,6 @@ When('I select a due date {int} days in the future', async function (days: numbe
   await this.page.fill('#mat-input-1', futureDate);
 });
 
-Then('Create ToDo button should be disabled', async function () {
+Then('Create ToDo button should be disabled', async function (this:CustomWorld) {
   await expect(this.page.getByRole('button', { name: 'Create ToDo' })).toBeDisabled();
 });
--- src/tests/steps/todoListSteps.ts
+++ src/tests/steps/todoListSteps.ts
@@ -1,32 +1,33 @@
-import { Given, Then, When, setDefaultTimeout } from "@cucumber/cucumber"
+import { Given, Then, When } from "@cucumber/cucumber"
 import { expect } from "@playwright/test";
+import { CustomWorld } from "../support/world";
 
-When('I click on three-dot icon', async function () {
+When('I click on three-dot icon', async function (this:CustomWorld) {
   await this.page.locator(`//tr[1]/td[4]/button`).click();
 });
 
-When('click on Delete button', async function () {
+When('click on Delete button', async function (this:CustomWorld) {
   await this.page.getByText('Delete', { exact: true }).click();
 });
 
 
-Then('the newly added {string} items should be removed', async function (text: string) {
+Then('the newly added {string} items should be removed', async function (this:CustomWorld,text: string) {
   await expect(this.page.locator(`text=${text}`)).toHaveCount(0);
 });
 
 
-Then('ToDo list should be sorted by due date ascending order', async function () {
-  const dateTexts: string[] = await this.page.locator('td.cdk-column-dueDate').allTextContents();
-  const actualDates = dateTexts.map(text => new Date(text.trim()).getTime());
+Then('ToDo list should be sorted by due date ascending order', async function (this:CustomWorld) {
+  const date: string[] = await this.page.locator('td.cdk-column-dueDate').allTextContents();
+  const actualDates = date.map(text => new Date(text.trim()).getTime());
   const sortedDates = [...actualDates].sort((a, b) => a - b);
   expect(actualDates).toEqual(sortedDates);
 });
 
-When('click on Archive button', async function () {
+When('click on Archive button', async function (this:CustomWorld) {
   await this.page.getByText('Archive', { exact: true }).click();
 });
 
-Then('the newly added {string} items should be displayed under Archive list', async function (text:string) {
+Then('the newly added {string} items should be displayed under Archive list', async function (this:CustomWorld,text:string) {
   const archivedHeader = await this.page.locator('.archived');
   const grabbedText = await archivedHeader.locator(`text="${text}"`).textContent();
   expect(grabbedText).toContain(text);;
